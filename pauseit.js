b9e698f833b765bba7ef6e128241f7c039f355e7
Marie Etong <m6rief@gmail.com>
2022-07-06 14:17:18 +0100

    added song and song controller

--- pauseit.js
+++ pauseit.js
@@ -1,15 +1,50 @@
 const timer = document.getElementById("twoMinutesTimer");
 const congratulations = document.querySelector(".congratulations");
+const tryAgain = document.querySelector(".tryAgain");
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+const audio = new Audio('../../../Downloads/Beautiful Piano Music - Relaxing Music, Study Music, Stress Relief, Sleep Music (Dior).mp3');
 
-let n = 120;
+// add button
+// make play() and stop() work
+// add event listener linked to the button that will start the timer and the song
+// update the current event listener so that the song stops when movement
+
+let n = 10;
+// let n = (10).toHHMMSS;
+
 let playingTime = setTimeout(countDown,0);
+
+function playAudio() {
+  audio.play();
+}
+
+function stopAudio() {
+  audio.pause();
+}
+
+// String.prototype.toHHMMSS = function () {
+//   var sec_num = parseInt(this, 10); // don't forget the second param
+//   var hours   = Math.floor(sec_num / 3600);
+//   var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+//   var seconds = sec_num - (hours * 3600) - (minutes * 60);
+
+//   if (hours   < 10) {hours   = "0"+hours;}
+//   if (minutes < 10) {minutes = "0"+minutes;}
+//   if (seconds < 10) {seconds = "0"+seconds;}
+//   return hours+':'+minutes+':'+seconds;
+// }
+// alert("120".toHHMMSS());
+
 
 function countDown(){
   n--;
   timer.innerHTML = `${n}`
+  tryAgain.innerHTML = ``
   if(n > 0){
-    clearTimeout(playingTime)
+    clearTimeout(playingTime);
     playingTime = setTimeout(countDown,1000);
+    playAudio();
   }
   else if(n==0){
     // window.location.replace("congratulations.html")
@@ -20,8 +55,10 @@ function countDown(){
 
 function stop() {
   clearTimeout(playingTime);
-  n = 120;
+  n = 10;
+  stopAudio();
   console.log("stop");
+  tryAgain.innerHTML = `Try again`
 }
 
 // EVENT LISTENERS
@@ -38,11 +75,15 @@ function stop() {
 // });
 
 document.addEventListener("mousemove", () => {
-  n = 120;
+  n = 10;
   countDown();
+  stopAudio();
+  tryAgain.innerHTML = `Try again`
 });
 
 document.addEventListener("mouseleave", () => {
-  n = 120;
+  n = 10;
   stop();
+  stopAudio();
+  tryAgain.innerHTML = `Try again`
 });
